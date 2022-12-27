7c626a3e22344a601f03de6ce26ac2875b9f5f69
Maycom Willams <maycom.willams@gmail.com>
2022-12-27 10:35:03 -0300

    Alterações na funcionalidade dos botões de ação

--- scripts.js
+++ scripts.js
@@ -10,12 +10,12 @@ const playButton = document.querySelector(".play")
 const stopButton = document.querySelector(".stop")
 const resetButton = document.querySelector(".reset")
 
-let mode = "easy"
-let INITIAL_SECONDS_AMOUNT = 300 // 5 minutes * 60 = 300 seconds
+let mode // = "easy"
+let INITIAL_SECONDS_AMOUNT // = 2 // 5 minutes * 60 = 300 seconds
 let secondsToDecress = INITIAL_SECONDS_AMOUNT;
 
 function selectMode(mode) {
-  console.log(mode)
+  console.log('mode selected:', mode)
   if (mode === 'easy') {
     return (
       mode = "easy",
@@ -23,7 +23,7 @@ function selectMode(mode) {
       secondsElement.innerHTML = "00",
       clearInterval(timer),
       playButton.removeAttribute('disabled'),
-      defineMode()
+      convertMinutesToSeconds(5)
     )
   } else if (mode === 'medium') {
     return (
@@ -32,7 +32,7 @@ function selectMode(mode) {
       secondsElement.innerHTML = "00",
       clearInterval(timer),
       playButton.removeAttribute('disabled'),
-      defineMode()
+      convertMinutesToSeconds(15)
     )
   } else if (mode === 'hard') {
     return (
@@ -41,7 +41,7 @@ function selectMode(mode) {
       secondsElement.innerHTML = "00",
       clearInterval(timer),
       playButton.removeAttribute('disabled'),
-      defineMode()
+      convertMinutesToSeconds(25)
     )
   }
 }
@@ -53,25 +53,18 @@ function convertMinutesToSeconds(minutes) {
   )
 }
 
-function defineMode() {
-  if (mode === "easy") {
-    convertMinutesToSeconds(5)
-  } else if (mode === "medium") {
-    convertMinutesToSeconds(15)
-  } else if (mode === "hard") {
-    convertMinutesToSeconds(25)
-  }
-}
-
 let timer
 
 function startTimer() {
-  if (secondsToDecress != 0) {
+  if (secondsToDecress === undefined) {
+    alert("Choose a mode to start the timer")
+  }
+  else if (secondsToDecress != 0) {
     timer = setInterval(contdownTimer, 1000)
+    playButton.setAttribute('disabled', true)
   } else {
     alert("Choose a mode to start the timer")
   }
-  playButton.setAttribute('disabled', true)
 }
 
 
@@ -79,18 +72,20 @@ function contdownTimer() {
   if (secondsToDecress != 0) {
     let minutes = Math.floor(secondsToDecress / 60);
     let seconds = secondsToDecress % 60;
-    console.log(secondsToDecress)
+    console.log('contdown timer:', secondsToDecress)
     return (
       secondsToDecress = secondsToDecress - 1,
       minutesElement.innerHTML = String(minutes).padStart(2, '0'),
       secondsElement.innerHTML = String(seconds).padStart(2, '0')
     );
   } else if (secondsToDecress <= 0) {
-    console.log("acabou os segundos")
+    console.log("Seconds fineshed")
     return (
       minutesElement.innerHTML = '00',
       secondsElement.innerHTML = '00',
       clearInterval(timer),
+      playButton.removeAttribute('disabled'),
+      notificationAudio.volume = 0.2,
       notificationAudio.play()
     )
   }
@@ -103,18 +98,14 @@ function pauseTimer() {
   clearInterval(timer)
 }
 
-function stopTimer() {
-  console.log("timer stopped")
-  playButton.removeAttribute('disabled')
-  clearInterval(timer)
-  defineMode()
-}
-
 function resetTimer() {
   console.log("timer reseted")
   playButton.removeAttribute('disabled')
   clearInterval(timer)
-  defineMode()
+  mode = undefined
+  secondsToDecress = 0
+  INITIAL_SECONDS_AMOUNT = 0
   minutesElement.innerHTML = Math.floor(INITIAL_SECONDS_AMOUNT / 60).toString().padStart(2, "0")
   secondsElement.innerHTML = String(INITIAL_SECONDS_AMOUNT % 60).padStart(2, "0")
+  console.log('initial:', INITIAL_SECONDS_AMOUNT)
 }
